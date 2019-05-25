c78585303aba6afd338c128ea1d5f48611a42797
Akhmad Herdian <akhmad.herdian@gmail.com>
2019-05-25 14:25:47 +0800

    Fixing model User

--- app/User.php
+++ app/User.php
@@ -2,12 +2,39 @@
 
 namespace App;
 
-use App\User;
-use Illuminate\Database\Eloquent\Model;
+use Illuminate\Notifications\Notifiable;
+use Illuminate\Foundation\Auth\User as Authenticatable;
 
-class User extends Model
+class User extends Authenticatable
 {
-    protected $fillable = ['name', 'description', 'creator_id'];
+    use Notifiable;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'name', 'email', 'password',
+    ];
+
+    /**
+     * The attributes that should be hidden for arrays.
+     *
+     * @var array
+     */
+    protected $hidden = [
+        'password', 'remember_token',
+    ];
+
+    /**
+     * The attributes that should be cast to native types.
+     *
+     * @var array
+     */
+    protected $casts = [
+        'email_verified_at' => 'datetime',
+    ];
 
     public function getNameLinkAttribute()
     {
@@ -20,10 +47,5 @@ class User extends Model
         $link .= '</a>';
 
         return $link;
-    }
-
-    public function creator()
-    {
-        return $this->belongsTo(User::class);
     }
 }
