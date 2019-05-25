6f892c5604b396d7998f1bae834a9145aa083302
Akhmad Herdian <akhmad.herdian@gmail.com>
2019-05-25 15:24:18 +0800

    Add field role_id in migration users table

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -18,6 +18,7 @@ class CreateUsersTable extends Migration
             $table->string('name');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
+            $table->unsignedTinyInteger('role_id'); // 1:admin, 2:teacher, 3:student
             $table->string('password');
             $table->rememberToken();
             $table->timestamps();
